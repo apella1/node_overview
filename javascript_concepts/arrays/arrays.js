61e989ef2d8f5b04a8bbd0b48e9973a06383206c
apella1 <johncollons5@gmail.com>
2023-12-10 13:09:57 +0300

    rest and spread operators

--- javascript_concepts/arrays/arrays.js
+++ javascript_concepts/arrays/arrays.js
@@ -4,3 +4,22 @@ const sports = ["Soccer", "Football", "Basketball", "Hockey"];
 for (let sport of sports) {
   console.log(sport);
 }
+
+// rest and spread operators
+const newSports = [...sports];
+console.log(newSports === sports);
+
+// using Array.slice method to create a shallow copy of an array
+const copiedSports = sports.slice();
+console.log(copiedSports);
+
+// passing arrays as values i.e element in another array
+const challenges = [sports, "Dancing"];
+console.log(challenges);
+
+// ! rest parameters
+function toArray(...params) {
+  return params;
+}
+
+console.log(toArray(1, 3, 4, 5));
