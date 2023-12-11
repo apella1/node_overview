67ad13554719ad5890fd404ee2399df98fd7d33e
apella1 <johncollons5@gmail.com>
2023-12-11 18:10:54 +0300

    serve html files

--- express_server/app.js
+++ express_server/app.js
@@ -1,3 +1,4 @@
+const path = require("node:path");
 const bodyParser = require("body-parser");
 const express = require("express");
 const adminRoutes = require("./routes/admin");
@@ -7,6 +8,10 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(shopRoutes);
-app.use(adminRoutes);
+app.use("/admin", adminRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+});
 
 app.listen(3000);
--- express_server/routes/admin.js
+++ express_server/routes/admin.js
@@ -1,14 +1,13 @@
+const path = require("path");
 const express = require("express");
 
 const router = express.Router();
 
 router.get("/add-product", (req, res, next) => {
-  res.send(
-    "<form action='product' method='POST'><input type='text' name='title' placeholder='Test input'/><input type='submit' value='Submit' /></form>"
-  );
+  res.sendFile(path.join(__dirname, "../", "views", "add_product.html"));
 });
 
-router.post("/product", (req, res, next) => {
+router.post("/add-product", (req, res, next) => {
   console.log(req.body);
   res.redirect("/");
 });
