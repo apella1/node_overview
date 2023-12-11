e3d10629540a66fc5de31404518a30fcfe9b3edd
apella1 <johncollons5@gmail.com>
2023-12-11 09:38:36 +0300

    parsing request bodies

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Node JS
 
 ## Basics
+
+- How the web works
+- Creating a node server
+- The node lifecycle and event loop
+- Requests and responses
+- Redirecting requests
--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 const http = require("node:http");
+const fs = require("node:fs");
 
 const server = http.createServer((req, res) => {
-  const { url } = req;
+  const { url, method } = req;
   if (url === "/") {
     res.setHeader("Content-Type", "text/html");
     res.write("<html>");
@@ -11,6 +12,22 @@ const server = http.createServer((req, res) => {
     );
     res.write("</html>");
     // ! signifies the end of writing to the response object
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("message.txt", message);
+    });
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
     return res.end();
   }
   // res.setHeader("Content-Type", "text/html");
